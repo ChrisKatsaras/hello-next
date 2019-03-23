de544a7f9513e3fd5b3c08f4719e335346800fa4
Christopher Katsaras <chriskatsaras5@gmail.com>
2019-03-23 19:00:14 -0400

    Completed shared components section

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
+import Layout from '../components/MyLayout.js'
+
 export default function About() {
-    return (
-      <div>
-        <p>This is the about page</p>
-      </div>
-    )
-  }  
+  return (
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  )
+}
