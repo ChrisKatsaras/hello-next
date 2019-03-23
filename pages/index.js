f0f166e1c8f42d273a9e6dde453ee366c96fde02
Christopher Katsaras <chriskatsaras5@gmail.com>
2019-03-23 18:24:41 -0400

    Completed Navigation section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 const Index = () => (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
+  <div>
+    <Link href="/about">
+        <button>Go to About Page</button>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
 )
+
   
 export default Index
