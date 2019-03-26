80bb881188d5cf3a95edfbe3c4e653dc150e683a
Christopher Katsaras <chriskatsaras5@gmail.com>
2019-03-25 21:24:07 -0400

    Changes for Nelify test

--- pages/post.js
+++ pages/post.js
@@ -1,22 +1,42 @@
 import Layout from '../components/MyLayout.js'
-import fetch from 'isomorphic-unfetch'
+import { withRouter } from 'next/router'
+import Markdown from 'react-markdown'
 
-const Post = props => (
+export default withRouter(props => (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
-    <img src={props.show.image.medium} />
-  </Layout>
-)
+    <h1>{props.router.query.title}</h1>
+    <div className="markdown">
+      <Markdown
+        source={`
+          This is our blog post.
+          Yes. We can have a [link](/link).
+          And we can have a title as well.
 
-Post.getInitialProps = async function(context) {
-  const { id } = context.query
-  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
-  const show = await res.json()
+          ### This is a title
 
-  console.log(`Fetched show: ${show.name}`)
+          And here's the content.
+      `}
+        />
+      </div>
+      <style jsx global>{`
+        .markdown {
+          font-family: 'Arial';
+        }
 
-  return { show }
-}
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
 
-export default Post
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+      `}</style>
+    </Layout>
+))
